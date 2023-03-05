f017eec2ba912f9530c333d17dc210375b2a8e4e
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-05 18:42:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,80 @@
-// Import dependencies and set up http server
-const express = require("express");
-const bodyParser = require("body-parser");
-const crypto = require("crypto");
-const config = require("./config");
-const app = express().use(bodyParser.json());
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const config = require('./config');
 
-// Create the endpoint for your webhook
-app.post("/webhook", (req, res) => {
-  let body = req.body;
+const app = express();
 
-  console.log(`Received webhook:`);
-  console.dir(body, { depth: null });
-
-  // Verify that the callback came from Facebook.
-  try {
-    verifyRequestSignature(req, res, body);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(403);
-    return;
-  }
-
-  // Send a 200 OK response if this is a page webhook
-  if (body.object === "page") {
-    res.status(200).send("EVENT_RECEIVED");
-
-    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
-    let webhook_event = body.entry[0].messaging[0];
-    console.log(webhook_event);
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Webhook server is listening, port 3000');
 });
 
-// Add support for GET requests to our webhook
-app.get("/messaging-webhook", (req, res) => {
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {
+    // Parse the request body from the POST
+    let body = req.body;
 
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === config.verifyToken) {
-      // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+            // Get the webhook event. entry.messaging is an array, but
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
     } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
-  }
 });
 
-//Server Listener
-const port = process.env.PORT || 3000;
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-async function startServer() {
-  try {
-    await app.listen(port);
-    console.log(`Server listening on port ${port}`);
-  } catch (err) {
-    console.error(`Error starting server: ${err.message}`);
-    process.exit(1);
-  }
-}
-
-startServer();
-
-
-// Verify that the callback came from Facebook.
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature-256"];
-
-  if (!signature) {
-    console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
-  } else {
-    var elements = signature.split("=");
-    var signatureHash = elements[1];
-    var expectedHash = crypto
-      .createHmac("sha256", config.appSecret)
-      .update(buf)
-      .digest("hex");
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
+    // Check if a token and mode were sent
+    if (mode && token) {
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === config.verifyToken) {
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Respond with 403 Forbidden if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
+});
+
+// Verify that the callback came from Facebook
+function verifyRequestSignature(req, res, buf) {
+    let signature = req.headers['x-hub-signature'];
+
+    if (!signature) {
+        console.error("Couldn't validate the signature.");
+    } else {
+        let elements = signature.split('=');
+        let signatureHash = elements[1];
+
+        let expectedHash = crypto.createHmac('sha1', config.appSecret)
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash !== expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
 }
+
+// Use body-parser middleware to parse request body
+app.use(bodyParser.json({
+    verify: verifyRequestSignature,
+}));
