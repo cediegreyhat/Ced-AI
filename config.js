d20f49a77b161ed95e3714e5156756f7c5a9a5d5
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-05 22:53:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
+
+const openai = require('openai');
+
+const fbVerifyToken = process.env.FB_VERIFY_TOKEN;
+const fbAccessToken = process.env.FB_ACCESS_TOKEN;
+const openaiApiKey = process.env.OPENAI_API_KEY;
+const port = process.env.PORT || 3000;
+
+const openaiClient = new openai.default({ apiKey: openaiApiKey });
+
 module.exports = {
-  pageAccessToken: process.env.PAGE_ACCESS_TOKEN,
-  verifyToken: process.env.VERIFY_TOKEN,
-  appSecret: process.env.APP_SECRET
+  fbVerifyToken,
+  fbAccessToken,
+  openaiClient,
+  port,
 };
