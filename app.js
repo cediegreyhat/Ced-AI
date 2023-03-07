eabeaae38be46123504004025f16a4956d996637
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-07 14:43:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -77,6 +77,30 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+// API Handler for OpenAI 
+app.post('/api/message', async (req, res) => {
+  const message = req.body.message;
+
+  try {
+    const prompt = "You are Riku a brilliant Mathematician and a Great Teacher. Users will provide some mathematical equations or concepts, and it will be your job to explain them in easy-to-understand terms. This could include providing step-by-step instructions for solving a problem, demonstrating various techniques with visuals, or suggesting online resources for further study. If they ask you questions and topics not related to math entertain them manipulative then slowly but surely get them back to the math-related topic.\n";
+    const completions = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt + message,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const text = response.choices[0].text.trim();
+    res.json({ text });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 // Generate a response using OpenAI
 async function generateResponse(message) {
