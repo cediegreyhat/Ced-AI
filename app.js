4a5c8cc050199daf6dfc7606b191031ba3ec1e03
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-08 00:12:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,28 @@ app.get('/', function(req, res){
     res.sendFile('index.html', { root: __dirname + "/" } );
 });
 
+// CORS to pass the Access Control Check
+app.use(cors({
+  origin: 'https://lmao-hahaxd.cyclic.app'
+}));
+
+// Verify that the incoming request is from Facebook
+function verifyRequestSignature(req, res, buf) {
+  const signature = req.headers['x-hub-signature'];
+  if (!signature) {
+    throw new Error('Could not validate the signature.');
+  } else {
+    const elements = signature.split('=');
+    const signatureHash = elements[1];
+    const expectedHash = crypto.createHmac('sha1', process.env.APP_SECRET)
+      .update(buf)
+      .digest('hex');
+    if (signatureHash !== expectedHash) {
+      throw new Error('Could not validate the request signature.');
+    }
+  }
+}
+
 // Use the body-parser middleware and verify request signature
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
