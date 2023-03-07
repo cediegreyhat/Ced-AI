626747d55c78209952e2d0305739544f2008d97a
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-07 17:05:18 +0800

    Update apihandler.js

--- apihandler.js
+++ apihandler.js
@@ -15,29 +15,18 @@ form.addEventListener('submit', async (event) => {
   }
 
   try {
-    const response = await axios.post('/api/message', { message });
+    // Send message to API
+    const response = await axios.post('https://your-api-url.com/messages', { message });
+
+    // Display response in chat box
     const text = response.data.text;
     const div = document.createElement('div');
     div.innerText = text;
-    div.classList.add('user-message');
+    div.classList.add('chat-message');
     chatBox.appendChild(div);
+
   } catch (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      if (error.response.status === 401) {
-        alert('You are not authorized to perform this action.');
-      } else if (error.response.status === 404) {
-        alert('The resource you are trying to access could not be found.');
-      } else {
-        alert('An error occurred. Please try again later.');
-      }
-    } else if (error.request) {
-      // The request was made but no response was received
-      alert('No response received from the server. Please try again later.');
-    } else {
-      // Something else happened while setting up the request
-      alert('An error occurred. Please try again later.');
-    }
+    console.error(error);
+    alert('An error occurred. Please try again later.');
   }
 });
