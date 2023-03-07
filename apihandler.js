f1c0dc0100e109159773c89dc9dd5e944bf76b9c
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-07 19:08:00 +0800

    Update apihandler.js

--- apihandler.js
+++ apihandler.js
@@ -3,17 +3,8 @@ const form = document.querySelector('#message-form');
 const messageInput = document.querySelector('#message-input');
 const chatBox = document.querySelector('#chat-box');
 
-// Function to display a chat message
-function displayChatMessage(text, isUser) {
-  const div = document.createElement('div');
-  div.innerText = text;
-  div.classList.add('chat-message');
-  if (isUser) {
-    div.classList.add('user-message');
-    div.classList.remove('chat-message');
-  }
-  chatBox.appendChild(div);
-}
+// Cache object to store responses
+const cache = {};
 
 // Event listener for form submission
 form.addEventListener('submit', async (event) => {
@@ -27,34 +18,28 @@ form.addEventListener('submit', async (event) => {
   }
 
   try {
-    // Send message to API
-    const response = await axios.post('https://rose-repulsive-indri.cyclic.app/api/message', { message });
+    let response;
+
+    // Check if response is cached
+    if (cache[message]) {
+      console.log('Using cached response for message:', message);
+      response = cache[message];
+    } else {
+      // Send message to API
+      response = await axios.post('https://rose-repulsive-indri.cyclic.app/api/message', { message });
+      // Cache the response
+      cache[message] = response;
+    }
 
     // Display response in chat box
     const text = response.data.text;
-    displayChatMessage(text, false);
+    const div = document.createElement('div');
+    div.innerText = text;
+    div.classList.add('chat-message');
+    chatBox.appendChild(div);
 
   } catch (error) {
     console.error(error);
     alert('An error occurred. Please try again later.');
   }
 });
-
-// Function to initialize the chat
-async function initChat() {
-  try {
-    // Send a welcome message to the API
-    const response = await axios.post('https://rose-repulsive-indri.cyclic.app/api/message', { message: 'hi' });
-
-    // Display the response in the chat box
-    const text = response.data.text;
-    displayChatMessage(text, false);
-
-  } catch (error) {
-    console.error(error);
-    alert('An error occurred. Please try again later.');
-  }
-}
-
-// Initialize the chat when the page loads
-initChat();
