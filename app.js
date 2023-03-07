e2c4b43e53fb6b2ab0512c076f68b5c109607067
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-07 15:27:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -82,39 +82,6 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-// API Handler for OpenAI 
-app.post('/api/message', async (req, res) => {
-  const message = req.body.message;
-
-try {
-    const prompt = "You are Riku a brilliant Mathematician and a Great Teacher. Users will provide some mathematical equations or concepts, and it will be your job to explain them in easy-to-understand terms. This could include providing step-by-step instructions for solving a problem, demonstrating various techniques with visuals, or suggesting online resources for further study. If they ask you questions and topics not related to math entertain them manipulative then slowly but surely get them back to the math-related topic.\n";
-    const completions = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt + message,
-      temperature: 0.7,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-
-    if (!completions || completions.status !== 200 || !completions.data || !completions.data.choices) {
-      console.log('OpenAI API response:', completions);
-      throw new Error('Failed to generate response.');
-    }
-
-    const responseText = completions.data.choices[0].text.trim();
-
-    console.log('Generated response:', responseText);
-
-    return responseText;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Failed to generate response.');
-  }
-}
-});
-
 
 // Generate a response using OpenAI
 async function generateResponse(message) {
