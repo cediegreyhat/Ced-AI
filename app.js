65a64f94a7e24048787d12119d38efa696420edb
cedie <cediegreyhat@gmail.com>
2023-03-10 00:24:01 +0800

    bug fixed

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ app.post('/webhook', async (req, res) => {
       console.log('Received standby event:', standby);
 
       // Check if there are user queries/questions in the standby event
-      if (standby.length > 0 && standby[0].message && standby[0].message.text) {
-        const userMsg = standby[0].message.text;
+      if (standby.length > 0 && (standby[0].message && standby[0].message.text) || (standby[0].text)) {
+        const userMsg = standby[0].message?.text || standby[0].text;
         const userId = standby[0].sender.id;
 
         // Check cache for previous response
@@ -142,32 +142,6 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-
-
-// API Endpoint for OpenAI Communication
-app.post('/api/message', async (req, res) => {
-  try {
-    const { message } = req.body.object || {};
-
-    // Check if message is present in request body
-    if (!message) {
-      return res.status(400).json({ error: 'Message is missing from request body.' });
-    }
-
-    // Validate message
-    if (typeof message !== 'string' || message.trim().length === 0) {
-      return res.status(400).json({ error: 'Invalid message format.' });
-    }
-
-    // Generate response
-    const response = await generateResponse(message);
-
-    res.json({ success: true, response });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to generate response.' });
-  }
-});
 
 // Generate responses using OpenAI
 async function generateResponse(message) {
