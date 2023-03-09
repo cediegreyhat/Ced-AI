b4991d793bc9cc8ddd2b958b890353175d6260ec
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-10 05:04:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -21,31 +21,6 @@ const app = express();
 
 openai.apiKey = process.env.OPENAI_API_KEY;
 
-// API Endpoint for OpenAI Communication
-app.post('/api/message', async (req, res) => {
-  try {
-    const { message } = req.body.object || {};
-
-    // Check if message is present in request body
-    if (!message) {
-      return res.status(400).json({ error: 'Message is missing from request body.' });
-    }
-
-    // Validate message
-    if (typeof message !== 'string' || message.trim().length === 0) {
-      return res.status(400).json({ error: 'Invalid message format.' });
-    }
-
-    // Generate response
-    const response = await generateResponse(message);
-
-    res.json({ success: true, response });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to generate response.' });
-  }
-});
-
 // Log all calls and request for debugging
 app.use((req, res, next) => {
   console.log('Received request:', req.method, req.url, req.body);
@@ -169,6 +144,31 @@ app.post('/webhook', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
+  }
+});
+
+// API Endpoint for OpenAI Communication
+app.post('/api/message', async (req, res) => {
+  try {
+    const { message } = req.body.object || {};
+
+    // Check if message is present in request body
+    if (!message) {
+      return res.status(400).json({ error: 'Message is missing from request body.' });
+    }
+
+    // Validate message
+    if (typeof message !== 'string' || message.trim().length === 0) {
+      return res.status(400).json({ error: 'Invalid message format.' });
+    }
+
+    // Generate response
+    const response = await generateResponse(message);
+
+    res.json({ success: true, response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to generate response.' });
   }
 });
 
