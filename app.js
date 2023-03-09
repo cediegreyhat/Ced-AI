0c0cb4ebde456bd8b99d8efe5da67904adcfd4fd
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-10 03:55:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ app.get('/', function(req, res){
 // CORS to pass the Access Control Check
 app.use(cors({
   origin: 'https://lmao-hahaxd.cyclic.app'
+  origin: 'https://rose-repulsive-indri.cyclic.app'
 }));
 
 // If the request is from facebook Verify it
