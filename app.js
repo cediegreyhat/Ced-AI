ff7a7ed392bdf12556e42442a02f13c2ec8143d1
cedie <cediegreyhat@gmail.com>
2023-03-10 01:56:33 +0800

    modified:   app.js

--- app.js
+++ app.js
@@ -146,6 +146,45 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
+// Verify webhook token with Facebook
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+    console.log('Webhook verified!');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// API Endpoint for OpenAI Communication
+app.post('/api/message', async (req, res) => {
+  try {
+    const { message } = req.body.object || {};
+
+    // Check if message is present in request body
+    if (!message) {
+      return res.status(400).json({ error: 'Message is missing from request body.' });
+    }
+
+    // Validate message
+    if (typeof message !== 'string' || message.trim().length === 0) {
+      return res.status(400).json({ error: 'Invalid message format.' });
+    }
+
+    // Generate response
+    const response = await generateResponse(message);
+
+    res.json({ success: true, response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to generate response.' });
+  }
+});
+
 // Generate responses using OpenAI
 async function generateResponse(message) {
   try {
