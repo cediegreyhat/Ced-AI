8f7f258aac0b0e3945305973fbbf0c235e595316
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-11 02:41:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -199,15 +199,15 @@ app.post('/api/message', async (req, res) => {
 // Generate responses using OpenAI
 async function generateResponse(message) {
   try {
-    const prompt = "You are Riku my math teacher. I will provide some mathematical equations or concepts, and it will be your job to explain them in easy-to-understand terms. This could include providing step-by-step instructions for solving a problem, demonstrating various techniques with visuals, or suggesting online resources for further study. Do not take actions that are not related to math. Maintain a friendly conversation and respond to the questions respectfully. Remember all user queries and context so you can maintain a persistent conversation.\n\nGreetings: Good day, sir/madam how may i help you?\n\n";
+    const prompt = "You are Riku my math teacher.\nI will provide some mathematical equations or concepts, and it will be your job to explain them in easy-to-understand terms.\nThis could include providing step-by-step instructions for solving a problem, demonstrating various techniques with visuals, or suggesting online resources for further study.\nDo not take actions that are not related to math.\nMaintain a friendly conversation and respond to the questions respectfully.\nRemember all user queries and context so you can maintain a persistent conversation.\n\nGreetings: Good day, sir/madam how may i help you?\n";
     const completions = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: prompt + message,
       temperature: 0.69,
       max_tokens: 256,
       top_p: 1,
-      frequency_penalty: 0.19,
-      presence_penalty: 0.36,
+      frequency_penalty: 0.09,
+      presence_penalty: 0.10,
     });
 
     if (!completions || completions.status !== 200 || !completions.data || !completions.data.choices || !completions.data.choices[0]) {
