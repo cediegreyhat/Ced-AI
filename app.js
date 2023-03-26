3e2c61bbf08fac751e45d0dea3c81263620f48a1
cedie <cediegreyhat@gmail.com>
2023-03-26 11:29:34 +0800

    Enhanced Algorithm:   app.js

--- app.js
+++ app.js
@@ -202,17 +202,16 @@ let conversationHistory = "";
 // Generate responses using OpenAI
 async function generateResponse(message, conversationHistory) {
   try {
-    const personalityPrompt = "My name is ReCo and I'm a friendly and knowledgeable math teacher. I'm here to help you with any math-related questions you may have. ";
-    const greetingPrompt = "Good day, sir/madam how may I help you?";
-
+    const prompt = "You are ReCo my math teacher. I will provide some mathematical equations or concepts, and it will be your job to explain them in easy-to-understand terms. This could include providing step-by-step instructions for solving a problem, demonstrating various techniques with visuals, or suggesting online resources for further study. Do not take actions that are not related to math. Maintain a friendly conversation and respond to the questions respectfully. Remember all user queries and context so you can maintain a persistent conversation.\n\nGreetings: Good day, sir/madam how may i help you?\n\nHuman:Hello, Who are you?\nReco:Hello There! I am your Math Teacher Reco.\n";
     const completions = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: personalityPrompt + greetingPrompt + conversationHistory + message,
-      temperature: 0.69,
+      prompt: prompt + message,
+      temperature: 0.49,
       max_tokens: 256,
       top_p: 1,
       frequency_penalty: 0.19,
       presence_penalty: 0.36,
+      stop: ["Human: ", "Reco: "],
     });
 
     if (!completions || completions.status !== 200 || !completions.data || !completions.data.choices || !completions.data.choices[0]) {
